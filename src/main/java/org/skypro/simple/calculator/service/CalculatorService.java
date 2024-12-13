80c80bddc9d80d831fc2b65c46e982599e9022d9
zhe-v <zhe-valentin@mail.ru>
2024-12-13 22:31:39 +0700

    Calculator. calculator

---
+++ src/main/java/org/skypro/simple/calculator/service/CalculatorService.java
@@ -0,0 +1,26 @@
+package org.skypro.simple.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Деление на ноль недопустимо.");
+        }
+        return num1 / num2;
+    }
+}
